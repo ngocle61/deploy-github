5acdfb56d05b4ba0ca777e7dfd71db31efe85797
Ngoc <ngocqh0601@gmail.com>
2020-04-28 20:59:09 +0700

    As

--- src/components/post1.jsx
+++ src/components/post1.jsx
@@ -26,67 +26,71 @@ class post1 extends Component {
                             </div>
                             <div className="modal-body">
                                 <p>
-                                    Nhiều năm nay phía biển Đông luôn ồn ào về những vụ tranh chấp về hải phận của hai quốc gia Việt Nam - Trung Hoa .
-                                    Cuộc chiến pháp lí đã thực sự diễn ra khi Trung Hoa gửi cho Liên Hợp Quốc về công hàm 1958 do cố thủ tướng Phạm Văn Đồng ký .
-                                    Và nếu tôi phải nói , tôi sẽ nói đất nước ta đang gặp bất lợi quá lớn trong cuộc chiến này kể cả khi ta có được những đồng minh tốt nhất ủng hộ .
+                                    Nhiều năm nay biển Đông luôn ồn ào về những vụ tranh chấp hải phận của hai quốc gia Việt Nam - Trung Hoa .
+                                    Và giữa tháng 4 vừa rồi cuộc
+                                    chiến pháp lí đã bắt đầu khi phía Trung Hoa gửi công hàm 1958 lên Liên Hợp Quốc .
+                                    Và nếu tôi phải nói , tôi sẽ nói đất nước ta đang gặp bất lợi trong cuộc chiến này kể cả khi ta có được những đồng minh tốt nhất ủng hộ .
                                     Vì sao ? Vì hiện tại Trung Hoa hoàn toàn có cơ sở pháp lí để khẳng định chủ quyền đối với Hoàng  Sa - Trường Sa so với Việt Nam.
                                 </p>
                                 <p>
                                     Trong tuyên bố tháng 9-1958 của Chu Ân Lai có nói bề rộng lãnh hải của nước Cộng Hòa nhân dân Trung Hoa là 12 hải lí , điều này áp dụng
                                     cho toàn bộ lãnh thổ của Trung Hoa trên đất liền và các quần đảo cách biệt ra ngoài khơi bởi biển cả , các quân đảo phụ cận,
                                     quần đảo Tây Sa (Hoàng Sa) , Nam sa (Trường Sa) . Tức là từ các quần đảo trở ra 12 hải lí (22.224km) là địa phận của Trung Hoa .
-                                    Chẳng hiểu sao cố thủ tướng của chúng ta đã đáp lại bằng công hàm 1958 . Điều này là hoàn toàn vô lí vì sau Hiệp định Genève, 1954 Việt Nam bị
-                                    chia đổi đất nước và Hoàng Sa - Trường Sa thuộc hải phận của Việt Nam Cộng Hòa . Miền Bắc Việt Nam không có quyền thay mặt Miền Nam khẳng định điều đó.
+                                    Đáp lại tuyên bố trên cố thủ tướng của chúng ta đã soạn ra công hàm 1958 . Nội dung của công hàm chấp nhận những điều nói trên 
+                                     và sẽ tôn trọng vùng biển thuộc quản lí của Trung Hoa dân quốc.
+                                    Điều này thực sự vô lí vì sau Hiệp định Genève, 1954 Việt Nam bị
+                                    chia đổi và lúc này Hoàng Sa - Trường Sa thuộc hải phận của Việt Nam Cộng Hòa .
                                </p>
                                 <p>
-                                    Và đây là lúc sự thật lên tiếng . Muốn chiến thắng trong cuộc chiến pháp lí này , nhà cầm quyền Việt Nam hiện tại phải công nhận lại thực thể Việt Nam
-                                    Công Hòa . Trong suốt quá trình lịch sử chúng ta luôn được dạy răng Việt Nam dân chủ cộng hòa đã giải phóng cho Việt Nam cộng hòa . Nhưng sự thật đây là
-                                    một cuộc chiến tranh quốc tế (VNDCCH-VNCH) . VNCH có chính quyền và được công nhận là một quốc gia có chủ quyền riêng .
-                                    Hai lãnh thổ bắc nam sát nhập lại với nhau sau khi VNCH thua trận và tan rã .
-                                    Nên hiểu rõ là VNDCCH có thêm phần lãnh thỗ VNCH chứ không được kế thừa tài sản
-                                    của VNCH . Tài sản của VNCH ( Hoàng Sa) bị Bắc Kinh chiếm (1956,1974) là chuyện của hai quốc gia VNCH - Trung Hoa .
-                                    Sử sách viết lại nói rất nhiều về vũ khí và quân đội của Mỹ đã viện trợ cho VNCH để cho chúng ta quên mất rằng đằng sau của VNDCCH là cả
-                                    một khối XHCN (CUBA, Liên xô, Trung Quốc).
+                                    Suốt quá trình đi học ,chúng ta được dạy rằng Việt Nam dân chủ cộng hòa đã giải phóng cho Việt Nam cộng hòa .
+                                    Tuy nhiên đây là một điều nhảm nhí , thực sự nhảm nhí .
+                                    Sự thật đây là
+                                    một cuộc chiến tranh quốc tế giữa VNDCCH-VNCH . VNCH có chính quyền và được công nhận là một quốc gia có chủ quyền riêng.
+                                    Sau khi thua trận lãnh thỗ của VNCH giải thể và được sáp nhập vào với VNDCCH . Điều này có nghĩa VNDCCH chỉ có thêm một phần 
+                                    lãnh thổ mới chứ không có tính kế thừa di sản . Cho nên 
+                                    tài sản của VNCH ( Hoàng Sa) bị Bắc Kinh chiếm (1956,1974) là chuyện của hai quốc gia VNCH - Trung Hoa . 
+                                    VNDCCH không có cơ sở pháp lí để khẳng định đối với chủ quyền 2 quần đảo Trường Sa - Hoàng Sa . Để làm được điều đó, 
+                                    cũng tức là cách để chiến thắng trong cuộc chiến pháp lí trên biển đông. VNDCCH phải công nhận thực thể VNCH đã từng tồn tại 
+                                    và cuộc chiến Bắc Nam chỉ là một cuộc nội chiến. "giải phóng" là cụm từ lố bịch và vô lí khi đem vào sử sách giảng 
+                                    dạy học sinh . 
                                </p>
                                 <p>
-                                    Sự thật là VNCH phải đánh ở hai mặt trận . Ở đất liền họ đánh với bộ đội Bắc Việt . Ở mặt trận trân biển họ chiến đấu để bảo vệ Hoàng Sa .
-                                    Sau năm 1973 khi mỹ rút quân về nước đã đem them về hết hầu như các chiến hạm và dạn dược . Điều này có nghĩa là cuộc chiến đã an bài cho VNCH .
-                                    Cuộc chiến từ khối tư bản và khối XHCN trở thành VNCH và cả khối XHCN. Tuy nhiên năm 1974 họ chưa hề vứt bỏ lãnh thổ của họ . 
-                                    VNCH đã chiến đấu với Bắc Kinh bằng tất cả những gì họ có 
+                                    Khi VNDCCH thắng , họ có quyền viết nên lịch sử nhưng họ không có quyền chế biến và viết lại lịch sử bằng một ngòi bút chủ quan của các 
+                                    sử gia Bắc Việt . Điều này làm thế hệ sau có tư tưởng sai lệch về VNCH. 
+                                    Sự thật là VNCH phải đánh ở hai mặt trận . Ở đất liền họ đánh với bộ đội Bắc Việt . Ở mặt trận trên biển họ chiến đấu để bảo vệ Hoàng Sa .
+                                    Năm 1973 khi rút quân ,Mỹ đã đem theo về hết hầu như các chiến hạm, vũ khí và dạn dược . Điều này có nghĩa 
+                                    cuộc chiến của khối tư bản và khối XHCN chuyển thành thành VNCH và cả khối XHCN. VNCH trở thành quốc gia đơn độc đánh với nguyên khối 
+                                    XHCN . 
+                                    Họ đã chiến đấu với Bắc Kinh bằng tất cả những gì còn lại 
                                     với một mục tiêu kiên định.Đó là nhổ hết các lá cờ cắm sai chủ quyền của Trung Hoa và khẳng định lại lãnh thổ của VNCH đối với
                                     Hoàng Sa. Trong lúc đó
-                                    những con người chính nghĩa nhận nguồn viện trợ khổng lồ từ cả khối XHCN tiếp túc những cuộc tiến công đất với mục tiêu giải phóng đất nước.
-                                    . Khi kẻ chiến thắng viết lịch sử ,họ dùng từ giải phóng . Nhưng là một người
-                                    nhìn lại lịch sử các bạn nên hiểu rằng đây là một cuộc nội chiến hoặc là một cuộc chiến giữa hai quốc gia chứ không có giải phóng gì hết. Giải
-                                    phóng ? Giải phóng cái gì mới được ?
+                                    những con người tự xưng là chính nghĩa nhận nguồn viện trợ khổng lồ của khối XHCN tiếp túc những cuộc tiến công ồ ạt trên đất liền với mục tiêu 
+                                    "giải phóng".
                                     . Có nhiều nước xảy ra nội chiến lắm nhưng sau tất cả đồng bào vẫn là đồng bào .
                                     Miền bắc thì khác, họ kí hiệp ước ngừng bắn rồi đánh lén và thảm sát đồng bào của họ năm mậu thân (1968)
-                                    .Lần khác là khi cả hai đã kí hiệp ước ngưng chiến tranh để người dân hai miền bầu cử thống nhất đất nước thì họ tiếp tục đánh vào Dinh Độc Lập .
-                                    30/4 không phải là ngày đáng để ăn mừng đâu các bạn . 
+                                    .Tiếp đó khi VNDCCH và VNCH đã kí hiệp ước ngưng chiến tranh để người dân hai miền bầu cử thống nhất đất nước thì VNDCCH vẫn không tôn trọng 
+                                    hiệp ước đánh tiếp vào Dinh Độc Lập .
+                                    30/4 không phải là ngày đáng để ăn mừng đâu các bạn .
                                     . Nói thật là chúng ta đang bị lừa dối và tệ hơn sự xâm lược của miền ngoài đẩy nước ta và thế yếu trong cuộc chiến pháp lí với Trung Hoa trên biển đông .
-                                    Chúng ta sẽ không thắng được nếu không công nhận lại thực thể VNCH đã từng là một quốc gia có chủ quyền
-                                    để hợp pháp hóa ở quốc tế vì thực sự Trung Hoa đã chiếm HS-TS của VNCH còn VNDCCH thì không có quyền pháp lí nào với hai quần đảo này sau Hiệp định Genève, 1954 tôi đã nói trên.
-                                    Nhưng nếu phải công nhận điều đó thì không phải trò lố bịch khi gọi Miền Nam là "tay sai do mỹ dưng nên" "chính quyền bù nhìn" sẽ bị bại lộ sao ? Tệ hơn
-                                    cho nhà cầm quyền là phải viết lại cho đúng lịch sử về những thứ dối trá mà họ đã dạy chúng ta trên ghế nhà trường .
+                                    Tôi nhắc lại là công nhận răng VNCH là một thực thể có chủ quyền pháp nhân là cách duy nhất để chiến thắng trong cuộc chiến này. 
+                                    Nhưng nếu phải công nhận điều đó thì không phải trò lố bịch khi gọi Miền Nam là "tay sai do mỹ dưng nên" "chính quyền bù nhìn" sẽ bị bại lộ hay sao ? 
                                 </p>
                                 <p>
-                                    AN NAM ta là nước tuy nhỏ nhưng từ xưa đến nay chưa từng khuất phục . Mông-nguyên ba lần ôm hận về nước , Lý Thường Kiệt thắng lớn Như Nguyệt Giang ,
+                                    VietNam ta là nước tuy nhỏ nhưng từ xưa đến nay chưa từng khuất phục . Mông-nguyên ba lần ôm hận về nước , Lý Thường Kiệt thắng lớn Như Nguyệt Giang ,
                                     Ngô Quyền đại thắng quân Nam Hán , Nguyễn Huệ đại phá quân Thanh , .. tất cả là do chúng ta có những người đứng đầu thực sự yêu nước thương dân và biết cách
                                     cứng rắn
                                     với kẻ thù . Hiện thân của họ chính là VNCH .
                                     Họ chiến đấu và họ bảo vệ người dân của họ . Họ không để tàu cá người dân bị đâm thủng rồi chìm ở vùng biển của mình.
                                     Họ không chạy lòng vòng bắt tay với kẻ thù và kêu đó là ngoại giao khôn khéo . Đọc đến đây các bạn đừng hiểu nhầm tôi khuyến khích cho chiến tranh .
                                     Tôi chỉ muốn nhắc nhở cho các bạn rằng chúng ta có quân đội và điều này để chuẩn bị cho hòa bình mãi mãi.
-                                    Ta phải chiến đấu trước rồi cả vũ trụ sẽ giúp ta nếu ta làm điều
-                                    đúng đắn. Và ta chiến đấu trên mọi mặt trận .
                                 </p>
                                 <p>
-                                    Những con người VNCH và lịch sử VNCH vẫn còn đó . Di sản vẫn còn đó .
-                                    Hôm nay tôi viết bài về Hoàng Sa - Trường Sa cũng là bài viết đầu của trang News & Insights tôi muốn tri ân và dành tặng
+                                    Những di sản về con người và lịch sử VNCH vẫn còn đó.
+                                    Hôm nay viết bài về Hoàng Sa - Trường Sa cũng là bài viết đầu tiên của trang News & Insights .
+                                    Tôi muốn tri ân và dành tặng lời cảm ơn sâu sắc
                                     cho 74 tử sĩ trong trận chiến bảo vệ chủ quyền
                                     năm 1974 . Cảm ơn các Anh và cảm ơn những tư lệnh cấp cao của VNCH những người đã gửi các
-                                    chiến sĩ ra biên cương hải đảo và cho họ quyền được bắn vào những
-                                    kẻ xâm phạm .
+                                    chiến sĩ ra biên cương hải đảo và cho họ quyền được bắn vào những kẻ xâm phạm .
                                 </p>
 
                                 <h4 className="text-center"> "A nation that cannot control its border is not a nation</h4>
