2e0c95e5bac7c56900f0e79b597a10a11546238a
Ngoc <ngocqh0601@gmail.com>
2020-04-28 21:20:40 +0700

    Ass

--- src/components/post3.jsx
+++ src/components/post3.jsx
@@ -23,10 +23,86 @@ class post3 extends Component {
                                 </button>
                             </div>
                             <div className="modal-body">
-                               
-                             </div>
-                            <div className="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Đóng</button>
+                                <img src="https://sc01.alicdn.com/kf/HTB1TeZjdrArBKNjSZFLq6A_dVXa8.jpg" alt="" />
+                                <p>
+                                    Đây là máy khâu con bướm của người Trung Hoa đã sản xuất ra cách đây khoảng 100 năm .
+                                    Sản phẩm này nhìn nó đơn giản, giờ đây có lẽ đối với người Việt Nam nó chỉ là còn hoài niệm xa xăm.
+                                </p>
+                                <p>
+                                    Thế nhưng, đối với những người làm và hiểu biết thực sự về khoa học công nghệ
+                                    thì nó là cột mốc và tiêu chuẩn quyết định cho một nền công nghiệp và sản xuất của một quốc gia.
+                                    Có một luật bất thành văn rằng: Chỉ 1 quốc gia có khả năng sản xuất 100% nội địa hóa được 1 cái máy khâu (với giá thành hợp lý)
+                                    thì khi ấy quốc gia đó mới đủ khả năng bước chân vào hàng ngũ những quốc gia có nền sản xuất công nghiệp phát triển.
+                                    Bởi máy khâu là mặt hàng gia dụng thiết yếu mà mọi người dân đều sử dụng,
+                                    ko những thế trong nó còn hàm chứa vô vàn những nền tảng căn bản của một nền sx công nghiệp cho tương lai
+                                    (để chế tạo những thứ phức tạp hơn như ô tô hay xe máy).
+                                </p>
+                                <p>
+                                    Để làm ra một cái máy khâu thì nền công nghiệp đó đòi hỏi phải có trình độ luyện kim - tức từ
+                                    quặng thép khai thác ở dưới đất tạo ra phôi thép.
+                                    Đây là giai đoạn cực kì quan trọng, bởi muốn có được một cái máy khâu chắc chắn, bền bỉ,
+                                    các chi tiết bên trong máy chống chịu được sự mài mòn khi vận hành; thì các chi tiết máy bên
+                                    trong ngay từ chi tiết rất nhỏ như cái mũi kim khâu cũng phải đạt được độ cứng sao cho hoạt động liên tục
+                                    (dập như máy khâu) hàng triệu triệu lần mà ko hỏng hóc hay gẫy vỡ. Nên nhớ hiện tại Vn vẫn nhập khẩu từ mũi kim,
+                                    sợi chỉ từ TQ chính vì chúng ta đã mất gốc ngay từ giai đoạn căn bản này rồi.
+                                </p>
+                                <p>
+                                    Sau đó tiếp tục đòi hỏi công nghệ cơ khí chính xác để gia công những phôi thép nói trên tạo hình thành cái máy khâu
+                                    hoàn chỉnh từ phần thân vỏ, tay quay, cho tới vô vàn những chi tiết nhỏ bên trong máy khâu.
+                                    Chính vì phải gia công làm sao ra dc những chi tiết nhỏ, mà sai số so với bản vẽ thiết kế rất thấp
+                                    nên người ta mới gọi là cơ khí chính xác.
+                                </p>
+                                <p>
+                                    Sau nữa, lại đến phần hoàn thiện đòi hỏi phải có công nghệ sơn làm sao tạo ra nước sơn mịn như mông em bé, bóng loáng,
+                                    chống chịu ăn mòn của môi trường,
+                                    thậm chí dùng 30-40 năm 1 cái máy khẫu mà nó vẫn ko bị han rỉ.
+                                </p>
+                                <p>
+                                    Tất cả các quốc gia như Đài Loan, TQ, Hàn Quốc, Nhật Bản đều đã có khả năng tự sx nội địa hóa 100%
+                                    cái máy khâu từ cách đây gần thế kỉ. Khi đã qua được bài test về trình độ công nghiệp đó;
+                                    lúc ấy họ mới có 2 nền tảng cần thiết là Luyện Kim và Cơ khí chính xác để bước chân vào những
+                                    lĩnh vực khó khằn và khủng khiếp hơn như đóng tàu, sx ô tô và các loại máy móc công nghệ cao.
+                                </p>
+                                <p>
+                                    Đây chính là lí do vì sao mà khi đọc báo, xem tivi các anh chị thấy chính phủ kêu gào phải bằng mọi giá nội địa hóa
+                                    các sp ô tô xe máy của các hãng nước ngoài lắp ráp ở VN như Toyota hay Honda. Thế nhưng mãi mãi khẩu hiệu chỉ
+                                    là khẩu hiệu, còn cái mục tiêu nội địa hóa vẫn mãi xa rời bởi đơn giản ko có doanh nghiệp phụ trợ nào ở
+                                    Vn đủ nền tảng căn bản để sx những linh kiện ô tô, xe máy đòi hỏi độ bền bỉ (nhằm đảm bảo an toàn khi vận hành
+                                    ) để cung cấp cho bọn Toyota hay Honda cả.
+                                    Mà muốn vươn lên thành cường quốc, đặt chân vào những nhóm quốc gia phát triển, thì điều kiện tất
+                                    yếu là phải có nền công nghiệp sx hùng mạnh; nếu ko Vn sẽ mãi lẹt đẹt ở cái bẫy thu nhập trung bình
+                                    và là quốc gia đang phát triển mà thôi. Mà nội địa hóa linh kiện là bước đi đầu tiên trong
+                                    tiến trình công nghiệp hóa (ko nội địa hóa dc là chết). Giờ cái nhà máy cơ khí Trần Hưng Đạo
+                                    - biểu tượng công nghiệp cơ khí lừng lẫy 1 thời của VN giờ thành Royal City rồi thì nội địa hóa bằng niềm tin à ?
+                                </p>
+                                <p>
+                                    Người Trung Quốc họ đã phải đi từng bước, từng giai đoạn tuần tự trong sx công nghiệp.
+                                    Đó là học làm công xưởng của thế giới, rồi đầu tư cho luyện kim, cơ khí chính xác tạo ra
+                                    biết bao nhiêu sản phẩm chất lượng, bền bỉ (thằng nào chửi đồ TQ chất lượng kém là ếch ngồi đáy giếng).
+                                    Sau đó họ mới nhảy vào lĩnh vực ô tô và chiến thắng vang dội khiến các ông lớn phải dè chừng; vì TQ có
+                                    khả năng tự sx các loại linh kiện của ô tô với giá thành vừa rẻ, chất lượng lại số lượng lớn
+                                    (tỉ lệ nội địa hóa của họ rất cao). Chỉ khi ấy ngành công nghiệp ô tô TQ mới tạo ra những chiếc ô
+                                    tô như dòng Zotye đang làm mưa, làm gió ở VN vì giá rẻ mà chất lượng hơn hẳn Hàn, Nhật (so với tầm giá).
+                                </p>
+                                <p>
+                                    Còn khi anh cố tình làm ô tô mà trong tay ko có luyện kim, ko có cơ khí chính xác,
+                                    ko có một nền công nghiệp nặng hùng mạnh đứng đằng sau để hỗ trợ; đến cây kim, cái máy khâu còn làm ko nổi ?
+                                    Như thế chắc chắn toàn bộ linh kiện của 1 cái ô tô "thương hiệu Việt" sẽ phải đi nhập từ nước ngoài; mà nhập toàn
+                                    bộ ko nội địa hóa được thì lấy đâu ra giá rẻ để người tiêu dùng ủng hộ ? Mà anh ko có khả năng
+                                    tự làm dc cái gì, ko nắm dc công nghệ gì.
+                                    Vậy làm sao có đủ kiến thức, trình độ kinh nghiệm để bảo dưỡng, sữa chữa một cái ô tô tiền tỉ trong suốt quá trình vận hành ?
+                                </p>
+                                <p>
+                                    Người TQ, người Hàn thành công vì họ làm ra cái ô tô giá rẻ,
+                                    chất lượng cao dành cho người dân của mình; sau rồi xuất khẩu ra nc ngoài kiếm ngoại tệ về cho quốc gia.
+                                    Còn người VN lại rất thành công vì dùng lòng yêu nước, tự hào dân tộc nhập nguyên con ô tô ngoại (gắn mác Việt)
+                                    bán với giá cắt cổ để lừa nhiều con gà vào chuồng móc tiền của nhau -chứ cái xe đắt như chó để lừa gà ở VN thôi,
+                                    xuất khẩu đi nc ngoài người ta cười cho vào mặt đấy. Đấy, cứ vậy mà sách xưa của bọn Pháp nó bảo tộc An Nam này
+                                    chỉ giỏi khôn lỏi, làm ăn mánh mung, lừa gạt lẫn nhau thì lại tự ái rồi bảo tự nhục cơ.
+                                </p>
+                                <div className="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Đóng</button>
+                                </div>
                             </div>
                         </div>
                     </div>
