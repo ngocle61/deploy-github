041756c083866d3922e272a1475c71d2863186e6
ngoc <ngocle61>
2022-04-04 03:17:11 +0700

    m

--- src/components/post7.jsx
+++ src/components/post7.jsx
@@ -7,13 +7,13 @@ class post7 extends Component {
         <div className="hovereffect1">
           <img
             className="img-responsive1"
-            src="https://bestwallpapers.in/wp-content/uploads/2018/04/cute-kids-kissing-monochrome-love-4k-wallpaper-800x600.jpg"
+            src="https://i.pinimg.com/originals/49/bf/bd/49bfbd2aa4ab5a6f641f10e047b2ce9c.jpg"
             alt=""
           />
           <div className="overlay1">
-            <h2>7 , 2 , 10</h2>
+            {/* <h2>Kết quả</h2> */}
             <button className="info1" data-toggle="modal" data-target="#modal7">
-              Đọc thêm{" "}
+              Đọc 
             </button>
           </div>
         </div>
@@ -40,25 +40,20 @@ class post7 extends Component {
               </div>
               <div className="modal-body">
                 <img
-                  src="https://www.thesun.co.uk/wp-content/uploads/2020/10/6f4f589f4b86d3db4eb588875f51bb69.jpg"
+                  src="https://www.tvinsider.com/wp-content/uploads/2018/11/DSCF2309_v2-1181248.jpg"
                   alt=""
                 />
+                {/* <p style={{ paddingTop: "30px" }}>
+                </p> */}
                 <p style={{ paddingTop: "30px" }}>
-                  7 điểm, dĩ nhiên không được như 10 điểm, và trong 3 điểm
-                  khoảng cách kia, người ta có thể liệt kê ra tùm lum chuyện
-                  không được của 7 điểm. Nhưng 7 điểm vẫn tốt hơn chán so với 2
-                  điểm. Nếu bạn nhìn vào chuyện 7 điểm và 2 điểm đều thua xa 10
-                  điểm, và gọi chúng chung một kiểu thì bạn lầm nặng. Có những
-                  sai lầm chẳng sao, ví dụ cái bánh 7 điểm và cái bánh 2 điểm,
-                  ăn nhầm chẳng chết ai.
+                 Thú thật mình vào đời khá sớm nên mình biết tiền rất là quan trọng, tuy không phải là quan trọng nhất, nhưng nó giải quyết được rất nhiều vấn đề. Kiếm được tiền là một hành trình rất vĩ đại, vì mình unlock được rất nhiều hiểu biết và khả năng trên con đường đó. Mình cũng nhìn thấy được phẩm chất của bản thân mình, có sẵn sàng vì cầm tiền mà thỏa hiệp đạo đức hay không, từ đó biết mà trau dồi. 
+                </p>
+                <p>Mình nhận ra những điều này từ khá sớm nên rất cố gắng để cả thiện bản thân .Thấm thoát cũng 2 năm bước chân vào cái nghề này. Càng học thấy càng ngu. Biết thế nên đêm nào cũng cố gắng ngồi với mấy quyến sách chuyên ngành vài ba chục phút. Buồn ngủ thì lướt facebook hoặc xem youtube cho tỉnh. Khổ cái từ hồi chuyển nhà về ở với thằng bạn chí cốt thì lại có những cuộc gặp gỡ mới . Thằng bạn mình cứ đi kiếm đâu ra info của mấy em nữ giới đẹp xinh. Thế là mình add fr nàng. Nàng không đẹp nhưng lại có nét hiền hoà thu hút khó tả. Mình cũng tò mò lắm muốn xem cái nét khả ái kia ngoài đời ra thế nào . Thế mà ba lần gặp nhau mình chỉ thấy mỗi cặp mắt và hai cái tai. Hôm nay mạnh dạn rủ nàng đi ăn lẩu. Mất đâu đó khoảng 1 tiếng cho nàng suy nghĩ nhưng kết quả giống như mình dự đoán. Chỉ là muốn confirm lại để sau này đỡ luyến tiếc. Cũng khá vui nhưng thật bất lịch sự khi cứ cố gắng bắt chuyện trong khi nàng đã khéo léo từ chối 2 lần.</p>
+                <p>
+                    Thế là mình biết mình lại phải học từ cái cách giao tiếp cho đến cách nhắn tin với những 'nàng' sau này.
                 </p>
                 <p>
-                  Nhưng nếu đó là sai lầm về suy nghĩ, hay nhận thức tầm cao
-                  hơn, và mình lại tin chính cái suy nghĩ sai lầm đó, như doanh
-                  nghiệp 7 điểm, ồ cũng như doanh nghiệp 2 điểm, hay lớn hơn như
-                  quốc gia 7 điểm, ồ cũng như quốc gia 2 điểm. Thì đôi khi bạn
-                  đang đánh cược chính cuộc đời mình để rồi xác suất cao là thua
-                  bạc và khát nước, không lối thoát.
+                    Cuối tuần mình nhận được tin vui từ công việc nhưng hôm nay biết là những niềm vui đó sẽ không có bóng hồng nào ở bên để chia sẽ cùng mình . Dù gì vẫn phải tập trung vào bản thân trước. Bí mật cuộc đời là ở chỗ, nhờ nhìn ra tất cả những sự tiêu cực trong đời này, cảm giác tích cực xuất hiện trong lòng người ta mới vững vàng. 
                 </p>
               </div>
               <div className="modal-footer">
